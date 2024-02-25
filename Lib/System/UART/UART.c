f7576f96cf9613c28ecaf9a82616dcee5e30dac4
MW <1176224694@qq.com>
2024-02-25 19:44:00 +0800

    更新了串口的库

--- Lib/System/UART/UART.c
+++ Lib/System/UART/UART.c
@@ -1,34 +1,41 @@
 #include "UART.h"
-
-extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart2;
-extern UART_HandleTypeDef huart3;
+#include "usart.h"
 
 uint8_t ch;
 uint8_t ch_r;
 
 // 重定向printf()函数到串口
 int fputc(int c, FILE * f) {
-	ch=c;
-	HAL_UART_Transmit(&huart1,&ch,1,1000);
+	ch = c;
+	HAL_UART_Transmit(&huart1, &ch, 1, UART_PRINTF_TIMEOUT);
 	return c;
 }
 
 // 重定向scanf()函数到串口
 int fgetc(FILE * F) {
-	HAL_UART_Receive (&huart1,&ch_r,1,0xffff);
+	HAL_UART_Receive(&huart1, &ch_r, 1, UART_PRINTF_TIMEOUT);
 	return ch_r;
 }
 
+#if UART_TX_DMA == ENABLE
+#define UART_Transmit			HAL_UART_Transmit_DMA
+#else
+#define UART_Transmit			HAL_UART_Transmit_IT
+#endif
+#if UART_RX_DMA == ENABLE
+#define UART_Receive			HAL_UARTEx_ReceiveToIdle_DMA
+#else
+#define UART_Receive			HAL_UARTEx_ReceiveToIdle_IT
+#endif
 
-uint8_t USART1_RxData[USART1_DataPackage_Length];
-uint8_t USART2_RxData[USART2_DataPackage_Length];
-uint8_t USART3_RxData[USART3_DataPackage_Length];
+uint8_t UART1_RxData[USART1_DataPackage_Length];
 
-void USART_Init(void) {
-	HAL_UARTEx_ReceiveToIdle_IT(&huart1, USART1_RxData, USART1_DataPackage_Length);
-	HAL_UARTEx_ReceiveToIdle_IT(&huart2, USART2_RxData, USART2_DataPackage_Length);
-	HAL_UARTEx_ReceiveToIdle_IT(&huart3, USART3_RxData, USART3_DataPackage_Length);
+void UART_Init(void) {
+	UART_Receive(&huart1, UART1_RxData, USART1_DataPackage_Length);
+}
+
+void UART1_SendData(uint8_t* Data, uint16_t Size) {
+	UART_Transmit(&huart1, Data, Size);
 }
 
 /**
@@ -41,28 +48,9 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
 	
 	if(huart == &huart1) {
 		
-		/* 自定义串口1功能代码 BEGIN */
-		HAL_UART_Transmit_IT(&huart1, USART1_RxData, Size);
-		/* 自定义串口1功能代码 END */
+		/* 插入串口处理代码 */
+//		UART_Transmit(&huart1, UART1_RxData, Size);
 
-		HAL_UARTEx_ReceiveToIdle_IT(&huart1, USART1_RxData, USART1_DataPackage_Length);
-	}
-
-	if(huart == &huart2) {
-
-		/* 自定义串口2功能代码 BEGIN */
-		HAL_UART_Transmit_IT(&huart2, USART2_RxData, Size);
-		/* 自定义串口2功能代码 END */
-
-		HAL_UART_Receive_IT(&huart2, USART2_RxData, USART2_DataPackage_Length);
-	}
-	
-	if(huart == &huart3) {
-
-		/* 自定义串口3功能代码 BEGIN */
-		HAL_UART_Transmit_IT(&huart3, USART3_RxData, Size);
-		/* 自定义串口3功能代码 END */
-
-		HAL_UART_Receive_IT(&huart3, USART3_RxData, USART3_DataPackage_Length);
+		UART_Receive(&huart1, UART1_RxData, USART1_DataPackage_Length);
 	}
 }
--- Lib/System/UART/UART.h
+++ Lib/System/UART/UART.h
@@ -1,12 +1,12 @@
-#ifndef UART_USART_H_
-#define UART_USART_H_
+#ifndef __UART_H
+#define __UART_H
 
 ///////////////////////////////////////////////////////////
 //
-// 文件功能：LED的相关功能函数
+// 文件功能：UART的相关功能函数
 // 版本：V1.0
 // 作者：墨蔚（MW）
-// 修改时间：2024/01/06
+// 修改时间：2024/02/23
 //
 // 注：已重写printf()函数
 //     具体功能实现在.c文件中的回调函数内改写
@@ -14,18 +14,29 @@
 ///////////////////////////////////////////////////////////
 
 #include "main.h"
-#include "stm32f1xx_hal.h"
+#include "stm32f4xx_hal.h"
 #include <stdio.h>
 
-/* USART数据包相关配置 */
-#define USART1_DataPackage_Length		10
-#define USART2_DataPackage_Length		10
-#define USART3_DataPackage_Length		10
+/* USART相关配置 */
+#define UART_TX_DMA										DISABLE
+#define UART_RX_DMA										DISABLE
+#define UART_PRINTF_TIMEOUT						1000
+#define USART1_DataPackage_Length			10
+
+extern uint8_t UART1_RxData[USART1_DataPackage_Length];
 
 /**
   * @brief 串口初始化
   * @return 无
   */
-void USART_Init(void);
+void UART_Init(void);
 
-#endif /* UART_USART_H_ */
+/**
+  * @brief 串口1发送数据
+  * @param Data 要发送的数据
+  * @param Size 要发送的数据长度
+  * @return 无
+  */
+void UART1_SendData(uint8_t* Data, uint16_t Size);
+
+#endif
